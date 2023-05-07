67ac9393fcb6b561e237ad12e6f1ba82318a5317
Al Zee <z@alz.ee>
2023-05-07 13:57:42 +0800

    nth

--- src/Controller/Admin/FaqCrudController.php
+++ src/Controller/Admin/FaqCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Node;
+use App\Entity\Region;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
@@ -12,9 +13,22 @@ use EasyCorp\Bundle\EasyAdminBundle\Orm\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class FaqCrudController extends AbstractCrudController
 {
+    private $doctrine;
+    private RequestStack $requestStack;
+    private $region;
+
+    public function __construct(ManagerRegistry $doctrine, RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+        $this->doctrine = $doctrine;
+        $this->region = $doctrine->getRepository(Region::class)->findOneBy(['label' => 'faq_list']);
+    }
+    
     public static function getEntityFqcn(): string
     {
         return Node::class;
@@ -31,7 +45,15 @@ class FaqCrudController extends AbstractCrudController
     public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
     {
         $response = $this->container->get(EntityRepository::class)->createQueryBuilder($searchDto, $entityDto, $fields, $filters);
-        $response->andWhere("entity.region = 15");
+        $id = $this->region->getId();
+        $response->andWhere("entity.region = $id");
         return $response;
+    }
+    
+    public function createEntity(string $entityFqcn)
+    {
+        $node = new Node();
+        $node->setRegion($this->region);
+        return $node;
     }
 }
