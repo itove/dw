bcc62d87266070eb3f46eba972dddbc6dfc0e243
Al Zee <z@alz.ee>
2023-05-08 17:35:51 +0800

    nth

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,12 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Conf;
 
 class SecurityController extends AbstractController
 {
+    private $doctrine;
+    
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+    
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        $conf = $this->doctrine->getRepository(Conf::class)->find(1);
+        
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
         // }
@@ -21,7 +32,14 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        // return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('@EasyAdmin/page/login.html.twig', [
+          'last_username' => $lastUsername,
+          'error' => $error,
+          'page_title' => $conf->getName(),
+          'csrf_token_intention' => 'authenticate',
+          'target_path' => $this->generateUrl('admin'),
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
