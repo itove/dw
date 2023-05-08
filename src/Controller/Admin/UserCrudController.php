76141116fdcd068a0d146dd38eb7e8dba6a99ef9
Al Zee <z@alz.ee>
2023-05-08 16:04:06 +0800

    nth

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -47,6 +47,7 @@ class UserCrudController extends AbstractCrudController
                 ->setChoices([
                     'user' => 'ROLE_USER',
                     'admin' => 'ROLE_ADMIN',
+                    'super_admin' => 'ROLE_SUPER_ADMIN',
                 ])
                 ;
             yield ChoiceField::new('roles')
