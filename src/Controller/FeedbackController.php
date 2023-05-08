6cff9fc57b8ff96fb73eb85b81963bc5e123a238
Al Zee <z@alz.ee>
2023-05-08 15:14:31 +0800

    nth

---
+++ src/Controller/FeedbackController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Feedback;
+use Doctrine\Persistence\ManagerRegistry;
+
+#[Route('/feedback')]
+class FeedbackController extends AbstractController
+{
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+    
+    #[Route('/list', name: 'app_feedback_list')]
+    public function index(): Response
+    {
+        return $this->render('feedback/index.html.twig', [
+            'controller_name' => 'FeedbackController',
+        ]);
+    }
+    
+    #[Route('/new', methods: ['POST'], name: 'app_feedback_new')]
+    public function new(Request $request): Response
+    {
+        $name = $request->request->get('name');
+        $phone = $request->request->get('phone');
+        $email = $request->request->get('email');
+        $title = $request->request->get('title');
+        $content = $request->request->get('content');
+        
+        $em = $this->doctrine->getManager();
+        $f = new Feedback();
+        $f->setName($name);
+        $f->setPhone($phone);
+        $f->setEmail($email);
+        $f->setTitle($title);
+        $f->setContent($content);
+        $em->persist($f);
+        $em->flush();
+        
+        return $this->json(['code' => 0]);
+    }
+}
